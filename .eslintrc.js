1fdfcc1d74aa1bba2e058097ea32a1dbea09170e
suhyun-kim9 <ksh020674@gmail.com>
2025-01-08 15:41:48 +0900

    기초세팅

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,11 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rules: {
+    'prettier/prettier': 0,
+    'no-unused-vars': 0, // 미사용 변수 경고
+    'react/self-closing-comp': 0, //
+    'react-native/no-inline-styles': 0, // 인라인스타일
+    'no-trailing-spaces': 0,
+  },
 };
