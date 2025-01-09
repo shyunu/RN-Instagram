30b5fafc8ff6f546c789858a428e31d5eb142640
suhyun-kim9 <ksh020674@gmail.com>
2025-01-09 12:48:11 +0900

    내프로필세팅

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,11 +1,76 @@
-import {View, Text} from 'react-native';
+import {View, Text, SafeAreaView, ScrollView} from 'react-native';
 import React from 'react';
+import ProfileBody from '../components/ProfileBody';
+import ProfileButton from '../components/ProfileButton';
+import Entypo from 'react-native-vector-icons/Entypo';
 
 const Profile = () => {
+  let circles = [];
+  let numberOfCircles = 10;
+
+  for (let index = 0; index < numberOfCircles; index++) {
+    circles.push(
+      <View key={index}>
+        {index === 0 ? (
+          <View
+            style={{
+              width: 60,
+              height: 60,
+              borderRadius: 100,
+              borderWidth: 1,
+              opacity: 0.7,
+              marginHorizontal: 5,
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <Entypo name="plus" style={{fontSize: 40, color: 'black'}} />
+          </View>
+        ) : (
+          <View
+            style={{
+              width: 60,
+              height: 60,
+              borderRadius: 100,
+              backgroundColor: 'black',
+              opacity: 0.1,
+              marginHorizontal: 5,
+            }}
+          />
+        )}
+      </View>,
+    );
+  }
+
   return (
-    <View>
-      <Text>Profile</Text>
-    </View>
+    <SafeAreaView style={{width: '100%', backgroundColor: 'white'}}>
+      <View style={{width: '100%', padding: 10}}>
+        <ProfileBody
+          name="shyunu"
+          accountName="shyunu"
+          profileImage={require('../../assets/images/userProfile.jpeg')}
+          followers="300"
+          following="35"
+          post="245"
+        />
+      </View>
+
+      <ProfileButton
+        id={0}
+        name="shyunu"
+        accountName="shyunu"
+        profileImage={require('../../assets/images/userProfile.jpeg')}
+      />
+
+      <ScrollView
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}
+        style={{
+          paddingVertical: 5,
+          paddingHorizontal: 10,
+        }}>
+        {circles}
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
